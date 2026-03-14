f272a3a389e1c84f1cde506e31d0b3809a22cffa
Ho Minh <minhhdph48234@fpt.edu.vn>
2026-03-14 14:09:25 +0700

    UserSeeder

---
+++ travel_tour/database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->insert([
+
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'user',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+            [
+                'name' => 'Guide',
+                'email' => 'guide@gmail.com',
+                'password' => Hash::make('123456'),
+                'role' => 'guide',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+
+        ]);
+    }
+}
