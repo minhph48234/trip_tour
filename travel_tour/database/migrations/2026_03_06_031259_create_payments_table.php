b8279bc5d3fe9eca5b39d55f30a5cda759e482f6
Ho Minh <minhhdph48234@fpt.edu.vn>
2026-03-14 11:18:59 +0700

    migration payments

---
+++ travel_tour/database/migrations/2026_03_06_031259_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('booking_id')
+                ->constrained('bookings')
+                ->cascadeOnDelete();
+
+            $table->enum('method',['cash','vnpay','momo']);
+
+            $table->decimal('amount',12,2);
+
+            $table->enum('status',['pending','paid','failed','refunded'])->default('pending');
+
+            $table->string('transaction_code')->nullable();
+
+            $table->timestamp('paid_at')->nullable();
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
