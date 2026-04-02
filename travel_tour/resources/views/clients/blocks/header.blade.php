079eefe87c05f465fbf250b8f5ed786541360b3c
Ho Minh <minhhdph48234@fpt.edu.vn>
2026-04-02 08:04:16 +0700

    update header - clients

--- travel_tour/resources/views/clients/blocks/header.blade.php
+++ travel_tour/resources/views/clients/blocks/header.blade.php
@@ -29,7 +29,10 @@
 
             <div class="relative group">
 
-                <button class="flex items-center gap-2 font-semibold text-gray-700 hover:text-blue-600">
+                <button class="flex items-center gap-2 px-3 py-2 rounded-lg 
+           font-semibold text-gray-700 
+           hover:bg-blue-100 hover:text-blue-700 
+           transition duration-200 cursor-pointer">
 
                     {{-- Avatar --}}
                     <div class="w-9 h-9 rounded-full bg-blue-600 text-white flex items-center justify-center">
